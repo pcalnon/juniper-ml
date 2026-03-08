fc0f9029cc7cb1826b6056a8bf70a7ee114bdbca
Paul Calnon <paul.calnon@gmail.com>
2026-03-07 20:46:27 -0600

    adding a new convenience script to run the wake_claude_code.bash script passing in sensible default params for an interactive session

---
+++ scripts/default_interactive_session_claude_code.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SCRIPT_PATH="$(dirname "$(realpath "${0}")")"
+
+DEFAULT_PROMPT="Hello World, Claude!"
+
+echo "Launching Default Interactive session with Claude Code"
+
+echo "${SCRIPT_PATH}/wake_the_claude.bash --id --worktree --dangerously-skip-permissions --effort high --prompt \"${DEFAULT_PROMPT}\""
+"${SCRIPT_PATH}"/wake_the_claude.bash --id --worktree --dangerously-skip-permissions --effort high --prompt "${DEFAULT_PROMPT}"
+
+echo "Closed Default Interactive session with Claude Code"
